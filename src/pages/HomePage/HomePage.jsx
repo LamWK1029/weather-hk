6815cadfcd0e1c94822cd071f8044ba4ccc7527a
LamWaiKin <kenlam@smartretail.co>
2024-05-21 17:30:36 +0800

    content

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import "./HomePage.css";
 import "../../styles/animations.css";
 
 const HomePage = () => {
   return (
     <div className="fade-in">
-      <h1>Home Page</h1>
+      <div className="home">
+        <video className="background-video" autoPlay loop muted playsInline>
+          <source
+            src="src/assets/weather-background/videos/raining.webm"
+            type="video/webm"
+          />
+          <source
+            src="src/assets/weather-background/videos/raining.mp4"
+            type="video/mp4"
+          />
+        </video>
+
+        <div className="slide-in main-content">
+          <h2>Tesung Kwan O</h2>
+          <h1>26 °C</h1>
+          <h3>Rainy</h3>
+          <h4>H: 26° L: 23°</h4>
+        </div>
+      </div>
     </div>
   );
 };
